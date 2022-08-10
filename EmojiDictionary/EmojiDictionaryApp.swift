caf82881cae289deafd1313fdb8062a3c36e2495
Devin Lynch <djlynch17@gmail.com>
2022-08-10 16:36:57 -0700

    Initial Commit

---
+++ EmojiDictionary/EmojiDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiDictionaryApp.swift
+//  EmojiDictionary
+//
+//  Created by Devin Lynch on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
